ec94861ef76121fa687fb5210102c15494ed1b63
lamatematica14 <sofia.marago@gmail.com>
2026-06-13 10:28:03 +0200

    Fix Astro config for GitHub Pages

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config'
+
+export default defineConfig({
+  site: "https://lamatematica14.github.io/personal_website/",
+  base: "/personal_website/",
+  output: "static",
+});
