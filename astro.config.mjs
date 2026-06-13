338c387b9149602cd1bc3632c7f756d5998b688b
lamatematica14 <sofia.marago@gmail.com>
2026-06-13 14:03:17 +0200

    Changed domain.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  site: "https://lamatematica14.github.io/personal_website/",
-  base: "/personal_website/",
+  site: "https://sofiamarago.it",
+  base: "/",
   output: "static",
 });
